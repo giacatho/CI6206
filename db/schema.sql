c8e571dad0f13cd58fd8173d55107eb533976574
Tin Nguyen <giacatho@gmail.com>
2015-08-23 08:54:54 +0800

    add db script.

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS t_users;
+CREATE TABLE t_users (
+    c_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    c_username VARCHAR(20) NOT NULL,
+    c_firstname VARCHAR(255) NOT NULL,
+    c_lastname VARCHAR(255) NOT NULL,
+    PRIMARY KEY(c_id),
+    UNIQUE(c_username)
+);
+
+ALTER TABLE t_users 
+    ADD c_password VARCHAR(20) NOT NULL;
---
+++ db/test_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO t_users(c_username, c_firstname, c_lastname) VALUES
+("tinnguyen", "Nguyen", "Tri Tin"),
+("michael", "Tan", "Michael"),
+("loke", "Mukherjee", "Lokenath"),
+("guojun", "Qiao", "Guo Jun");
+
+UPDATE t_users SET c_password='tinnguyen' WHERE c_username='tinnguyen';
+UPDATE t_users SET c_password='michael' WHERE c_username='michael';
+UPDATE t_users SET c_password='loke' WHERE c_username='loke';
+UPDATE t_users SET c_password='guojun' WHERE c_username='guojun';
+
